11ab8397d5df938ffe9c4fec90ed2741a0d8268b
poph55 <palmalama@gmail.com>
2017-12-05 08:18:31 -0500

    good change

--- greeting.py
+++ greeting.py
@@ -1,3 +1,8 @@
-name = str(input("What is your name?"))
+import sys
+name = sys.argv[1]
 
 print("Hello, " + name + "!")
+
+
+
+# I accidentally did the in-program asking of the name first, so the second one is the command line one. Sorry!
